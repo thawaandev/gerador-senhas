de9a7cb86af00de3394f6d14b3e87e5615cf14f3
Thawan Lucas <thawanlucascaze@hotmail.com>
2023-09-07 23:07:50 -0300

    New features

---
+++ index.js
@@ -0,0 +1,12 @@
+ function gerarSenha() {
+    const length = document.getElementById("tamanho").value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+     document.getElementById("senha").value = password;
+}
